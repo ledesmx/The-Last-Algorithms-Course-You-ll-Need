11a33560c09001e5bc17f8d804e81dcf17841308
Alberto Ledesma <alberto.ledesma.dev@gmail.com>
2024-03-03 17:02:18 -0600

    Binary search solved

--- search/binary_search.ts
+++ search/binary_search.ts
@@ -1,3 +1,20 @@
 export function binary_search(arr: number[], n: number): boolean {
+    let lo = 0;
+    let hi = arr.length;
+
+    while(lo < hi) {
+        let mid = Math.floor((lo + hi) / 2);
+        let midn = arr[mid];
+
+        if(midn === n) {
+            return true;
+        }
+        if(midn > n) {
+            hi = mid;
+        } else {
+            lo = mid + 1;
+        }
+    }
+
     return false;
 }
