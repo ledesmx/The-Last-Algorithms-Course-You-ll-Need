f6296be7f1a9941db67451e049e2051931fa4525
Alberto Ledesma <alberto.ledesma.dev@gmail.com>
2024-03-04 21:26:49 -0600

    Solve two crystals probles with O(log n)

--- search/two_crystal_balls.ts
+++ search/two_crystal_balls.ts
@@ -1,3 +1,22 @@
 export function two_crystal_balls(breaks: boolean[]): number {
+    let lo = 0;
+    let hi = breaks.length;
+
+    while(lo + 2 < hi) {
+        let mid = Math.floor((lo + hi) / 2);
+        if(breaks[mid]) {
+            hi = mid + 1;
+        } else {
+            lo = lo = mid + 1;
+        }
+    }
+    while(lo < hi) {
+        if(breaks[lo]) {
+            return lo;
+        } else {
+            ++lo;
+        }
+    }
+
     return -1;
 }
