0c08a1dfbf41c8b88bf6c88e2df0e5036513fa69
Alberto Ledesma <alberto.ledesma.dev@gmail.com>
2024-03-06 17:04:53 -0600

    Solver Two Crystal Balls with O(sqrt(n))

--- search/two_crystal_balls.ts
+++ search/two_crystal_balls.ts
@@ -1,4 +1,56 @@
+// Given two crystal balls that will break if dropped from high
+// enough distance, determine the exact spot in which it will 
+// break in the most optimized way.
+
 export function two_crystal_balls(breaks: boolean[]): number {
+    const jmpAmount = Math.floor(Math.sqrt(breaks.length));
+
+    let i = jmpAmount;
+    for(; i < breaks.length; i += jmpAmount) {
+        if(breaks[i]) {
+            break;
+        }
+    }
+
+    i -= jmpAmount;
+    for(; i < breaks.length; ++i) {
+        if(breaks[i]) {
+            return i;
+        }
+    }
+    
+    return -1;
+}
+
+export function two_crystal_balls_2(breaks: boolean[]): number {
+    const sqrt = Math.floor(Math.sqrt(breaks.length));
+    let high = 0;
+
+    while(high < breaks.length) {
+        if(breaks[high]) {
+            break;
+        }
+        high += sqrt;
+    }
+
+    if(high < sqrt) {
+        return high;
+    }
+    if(high >= breaks.length) {
+        return -1;
+    }
+
+    high -= sqrt;
+    while(!breaks[high]) {
+        ++high;
+    }
+
+    return high;
+}
+
+
+// Algorithm with unlimited crystal balls. With O(log n)
+export function crystal_balls(breaks: boolean[]): number {
     let lo = 0;
     let hi = breaks.length;
 
