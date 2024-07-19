91042a243da269690fd04b9141e25e11a2fbcad4
Alberto Ledesma <alberto.ledesma.dev@gmail.com>
2024-07-19 10:37:10 -0600

    Added #autocomplete method in Tries

--- heap/tries.ts
+++ heap/tries.ts
@@ -27,7 +27,43 @@ export class Trie {
     this.addWalk(word, wordIndex + 1, currentNode.keys[key]);
   }
   autocomplete(input: string): string | null {
-    return "";
+    const node = this.getLastNode(input);
+    if (node === null) {// Last Node does no exist in the Trie
+      return null;
+    }
+
+    const word = this.getFirstWordFromInput(input, node);
+    if (word === null) {// Not found any word
+      return null;
+    }
+    return word;
+  }
+  private getLastNode(word: string): TrieNode | null {
+    if (this.root.keys.length === 0) {
+      return null;
+    }
+    let lastNode = this.root;
+    for (let i=0; i < word.length; ++i) {
+      const key = getCharIndex(word, i);
+      if (lastNode.keys[key] === undefined) {
+        console.log(word, "null")
+        return null;
+      }
+      lastNode = lastNode.keys[key];
+    }
+    return lastNode;
+  }
+  private getFirstWordFromInput(input: string, currentNode: TrieNode): string | null {
+    if (currentNode.isWord) {// Found a possible word
+      return input;
+    }
+    for (let i=0; i < currentNode.keys.length; ++i) {
+      if (currentNode.keys[i] !== undefined) {
+        const char = getCharByIndexCode(i);
+        return this.getFirstWordFromInput(`${input}${char}`, currentNode.keys[i]);
+      }
+    }
+    return null;
   }
   getTrie(): TrieNode {
     const [...keys] = this.root.keys;
@@ -41,6 +77,11 @@ export function getCharIndex(word: string, wordIndex: number = 0): number {
   const zero = "a".charCodeAt(0);
   const index = word.charCodeAt(wordIndex) - zero;
   return index;
+}
+export function getCharByIndexCode(index: number): string {
+  const zero = "a".charCodeAt(0);
+  const char = String.fromCharCode(index + zero);
+  return char;
 }
 export function genTrieNode(isWord = false): TrieNode {
   const keys: TrieNode[] = [];
