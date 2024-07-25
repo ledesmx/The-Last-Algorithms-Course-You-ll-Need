10a1c1dae79d705e009b584e31ecc8bec5b0ffa1
Alberto Ledesma <alberto.ledesma.dev@gmail.com>
2024-07-24 22:26:48 -0600

    Added solution for DFS adjacency list

--- graphs/DFS_adjacency_list.ts
+++ graphs/DFS_adjacency_list.ts
@@ -6,5 +6,32 @@ export type WeightedAdjacencyList = Connection[][];
   
 
 export function dfs(graph: WeightedAdjacencyList, source: number, needle: number): number[] | null {
-  return [];
+  if (graph.length === 0) {
+    return null;
+  }
+  const seen = new Array(graph.length).fill(false);
+  const path = dfs_walk(graph, source, needle, seen, []);
+
+  return path;
+}
+function dfs_walk(graph: WeightedAdjacencyList, current: number, needle: number, seen: boolean[], path: number[]): number[] | null {
+  if (seen[current]) {
+    return null;
+  }
+  if (current === needle) {
+    path.push(current);
+    return path;
+  }
+
+  path.push(current);
+  seen[current] = true;
+
+  for (let i=0; i < graph[current].length; ++i) {
+    const result = dfs_walk(graph, graph[current][i].to, needle, seen, path);
+    if (result !== null) {
+      return result;
+    }
+  }
+  const p = path.pop();
+  return null;
 }
