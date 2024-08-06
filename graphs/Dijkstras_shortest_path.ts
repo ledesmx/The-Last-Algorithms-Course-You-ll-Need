a57bf8049b3257f3b31a804f30b24881c87eac1c
Alberto Ledesma <alberto.ledesma.dev@gmail.com>
2024-08-05 21:59:18 -0600

    Added auxiliar functions to get lowest unseen node and check for unvisited nodes in Dijkstras algorithm

--- graphs/Dijkstras_shortest_path.ts
+++ graphs/Dijkstras_shortest_path.ts
@@ -7,3 +7,24 @@ export type WeightedAdjacencyList = Connection[][];
 export function dijkstra_list(graph: WeightedAdjacencyList, source: number, sink: number): number[] | null {
   return [];
 }
+
+function getLowestUnseenNode(seen: boolean[], dist: number[]): number {
+  let lowest = -1;
+
+  for (let i=0; i<seen.length; ++i) {
+    if (!seen[i] && dist[i] < dist[lowest]) {
+      lowest = i;
+    }
+  }
+  
+  return lowest;
+}
+
+function hasUnvisitedNode(seen: boolean[]): boolean {
+  for (let i=0; i<seen.length; ++i) {
+    if (!seen[i]) {
+      return true;
+    }
+  }
+  return false;
+}
