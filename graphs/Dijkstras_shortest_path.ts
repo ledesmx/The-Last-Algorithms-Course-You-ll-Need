703736bdb3880564743c29bdd1675237ef0dd6f0
Alberto Ledesma <alberto.ledesma.dev@gmail.com>
2024-08-07 15:12:46 -0600

    Added Dijkstras shortest path solution

--- graphs/Dijkstras_shortest_path.ts
+++ graphs/Dijkstras_shortest_path.ts
@@ -5,14 +5,66 @@ type Connection = {
 export type WeightedAdjacencyList = Connection[][];
 
 export function dijkstra_list(graph: WeightedAdjacencyList, source: number, sink: number): number[] | null {
-  return [];
+  if (source >= graph.length, sink >= graph.length) {
+    return null;
+  }
+
+  const previous = new Array(graph.length).fill(-1);
+  const seen = new Array(graph.length).fill(false);
+  const distance = new Array(graph.length).fill(Infinity);
+
+  distance[source] = 0;
+
+  while (hasUnvisitedNode(seen)) {
+    const nearestNode = getNearestUnseenNode(seen, distance);
+    if (nearestNode === -1) {
+
+      break;
+    }
+
+    seen[nearestNode] = true; // mark node as seen
+    if (nearestNode === sink) {
+      break;
+    }
+
+    for (let i=0; i<graph[nearestNode].length; ++i) {
+      const edge = graph[nearestNode][i];
+      if (seen[edge.to] === true) {
+        continue; // omit the node when it is already marked as seen
+      }
+
+      const edgeDistance = edge.weight + distance[nearestNode];
+      if (edgeDistance < distance[edge.to]) {
+        distance[edge.to] = edgeDistance;
+        previous[edge.to] = nearestNode;
+      }
+    }
+  }
+
+  if (seen[sink] === false) {
+    return null;
+  }
+
+  const path: number[] = getPath(previous, source, sink);
+
+  return path;
 }
 
-function getLowestUnseenNode(seen: boolean[], dist: number[]): number {
+function getNearestUnseenNode(seen: boolean[], distance: number[]): number {
   let lowest = -1;
 
   for (let i=0; i<seen.length; ++i) {
-    if (!seen[i] && dist[i] < dist[lowest]) {
+    if (!seen[i] && distance[i] !== Infinity) {
+      lowest = i;
+      break;
+    }
+  }
+  if (lowest === -1) {
+    return -1;
+  }
+
+  for (let i=0; i<seen.length; ++i) {
+    if (!seen[i] && distance[i] < distance[lowest]) {
       lowest = i;
     }
   }
@@ -27,4 +79,18 @@ function hasUnvisitedNode(seen: boolean[]): boolean {
     }
   }
   return false;
+}
+
+function getPath(previous: number[], source: number, sink: number): number[] {
+  const path = getPathWalk(previous, source, sink, []);
+  path.reverse();
+  return path;
+}
+function getPathWalk(previous: number[], source: number, currentNode: number, path: number[]): number[] {
+  path.push(currentNode);
+  if (currentNode === source) {
+    return path;
+  }
+
+  return getPathWalk(previous, source, previous[currentNode], path);
 }
