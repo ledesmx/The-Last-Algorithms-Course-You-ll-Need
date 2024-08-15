76e818d82b9aae4a75ed1998fc80b37d47668e0f
Alberto Ledesma <alberto.ledesma.dev@gmail.com>
2024-08-15 16:17:28 -0600

    Add LRU class

---
+++ maps_and_lru/lru_cache.ts
@@ -0,0 +1,16 @@
+export class LRU<K, V> {
+  private length: number;
+  private size: number;
+
+  constructor(size: number) {
+    this.length = 0;
+    this.size = size;
+  }
+
+  update(key: K, value: V):void {
+    
+  }
+  get(key: K): V | undefined {
+    return undefined;
+  }
+}
